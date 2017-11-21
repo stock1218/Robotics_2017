6e60879cac43464678367abcf7e5280bef3f2f89
William N C Setten <33688252+WillSetten@users.noreply.github.com>
2017-11-21 11:00:01 +0000

    Update PickUp.java

--- Project/PickUp.java
+++ Project/PickUp.java
@@ -6,6 +6,7 @@ public class PickUp implements Behavior {
 		Motor.B.rotate(140,true);
 		Delay.msDelay(200);
 		pilot.stop();
+		main.map[(int)nav.getPoseProvider().getPose().getX()][(int)nav.getPoseProvider().getPose().getY()]=0;
 	}
 	public void suppress() {
 		
