462e04ed4a64932e23fcdce00c1014632c0afbe6
tomsoal <33701079+tomsoal@users.noreply.github.com>
2017-11-23 14:45:58 +0000

    Update Arrange.java
    programmed it to count number of bricks on the grid, will return to base before compiling algorithms, cases set up for each number of bricks, just need to finalise how the geometry and map will work, then just a case of adding each coordinates to a path and using the navigator to follow that path

--- Project/Arrange.java
+++ Project/Arrange.java
@@ -1,16 +1,61 @@
+import lejos.robotics.pathfinding.Path;
+
 public class Arrange implements Behavior {
-    
-    public void action() {
-    //understand where the robot is, where the bricks are in relation to the robot
-    //work out where the bricks have to go using grid
-    //use PickUp, PutDown classes to help deliver bricks
-    }
-    
-    public void suppress() {
-    
-    }
-    
-    public boolean takeControl() {
-        return Main.isCollected();
-    }
+
+	public void action() {
+		int count=0;
+		Path path = new Path();
+
+		//counts number of bricks
+		for(int i = 0;i < Main.map.length;i++)
+		{
+			for(int j = 0;j < Main.map[].length;j++) {
+				if (Main.map[i][j] == 1)  count += 1;
+			}
+		}
+		//hardcoded as 6 bricks
+		Main.pp.getPose();
+		path.add(new Waypoint(450,450));
+		Main.nav.followPath(path());
+		//travels to base
+		switch(count) {
+			case 0: 
+				
+				break;
+			case 1:
+				
+				break;
+			
+			case 2:	
+				
+				break;
+
+			case 3:
+				
+				break;
+
+			case 4:
+				
+				break;
+
+			case 5: 
+				
+				break;
+
+			case 6:
+				
+				break;
+
+		}
+		
+		
+	}
+	
+	public void suppress() {
+		
+	}
+	
+	public boolean takeControl() {
+		return Main.isCollected;
+	}
 }
