a790f28c6ea84408011cc6d1f1527dacfd72c28c
tomsoal <33701079+tomsoal@users.noreply.github.com>
2017-11-28 11:49:17 +0000

    Update Turn.java
    Minor bug fixes when compiled with whole system

--- Project/Turn.java
+++ Project/Turn.java
@@ -1,10 +1,12 @@
+import lejos.robotics.subsumption.Behavior;
+
 public class Turn implements Behavior {
 	//light sensor - black = 30, white = 50
 	//colour sensor colour id - white = 6, black = 7
 	//left hand side is colour sensor, right is light sensor
 	//implement method to ensure it knows which way to rotate
 	public void action() {
-
+		System.out.println("turn");
 		//functionality to straighten robot up
 		if ((Main.ls.readValue()<40)&&(Main.cs.getColorID()!=7)) {
 			do {
@@ -20,7 +22,7 @@ public class Turn implements Behavior {
 		Main.pilot.travel(120);
 		Main.pilot.arc(50, 250);
 		//check to ensure robot is straight before it proceeds
-		if ((Main.ls.readValue()>40)&&(Main.cd.getColorID() ==7) { //right side behind
+		if ((Main.ls.readValue()>40)&&(Main.cs.getColorID() ==7)) { //right side behind
 			do {
 				Main.pilot.rotate(-10);
 			} while (Main.ls.readValue()>40);
