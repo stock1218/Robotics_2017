fe506d6c04c6bcce3d878f8683f3fc11f8388073
tomsoal <33701079+tomsoal@users.noreply.github.com>
2017-11-30 15:36:12 +0000

    Update Adjust.java
    work in progress, as far as we got 30/11

--- Project/Adjust.java
+++ Project/Adjust.java
@@ -1,32 +1,41 @@
 import lejos.robotics.subsumption.Behavior;
 
-
 public class Adjust implements Behavior {
-	//light sensor - black = 30, white = 50
-	//colour sensor colour id - white = 6, black = 7
-	//left hand side is colour sensor, right is light sensor
+	// light sensor - black = 30, white = 50
+	// colour sensor colour id - white = 6, black = 7
+	// left hand side is colour sensor, right is light sensor
 	public void action() {
+		Main.turning = true;
 		System.out.println("turn");
-		if ((testtt.ls.readValue()<40)&&(testtt.cs.getColorID()!=7)) {
-				testtt.pilot.travel(-15);
-				testtt.pilot.rotate(5);
-				testtt.pilot.travel(15);
+		float h = Main.nav.getPoseProvider().getPose().getHeading();
+		if ((Main.ls.readValue() < 40) && (Main.cs.getColorID() != 7)) {
+//			while ((Main.cs.getColorID() != 7) && (Main.ls.readValue() < 40)) {
+				Main.pilot.rotate(1);
+
+	//		}
+		} else if ((Main.ls.readValue() > 40) && (Main.cs.getColorID() == 7)) {
+		//	while (Main.ls.readValue() > 40 && (Main.cs.getColorID() == 7)) {
+				Main.pilot.rotate(1);
+			//}
 		}
-		else if ((testtt.ls.readValue()>40)&&(testtt.cs.getColorID()==7))
-				testtt.pilot.travel(-15);
-				testtt.pilot.rotate(-5);
-				testtt.pilot.travel(15);
+
+		if ((Main.ls.readValue() < 40) && (Main.cs.getColorID() == 7)) {
+			Main.adjusted = true;
+			Main.pilot.stop();
+		}
+		Main.nav.getPoseProvider().getPose().setHeading(h);
 	}
+
 	public void suppress() {
-		
-	}	
+
+	}
+
 	public boolean takeControl() {
-		return (
-				(testtt.ls.readValue()<40) && (testtt.cs.getColorID() != 7)
-					||
-				(testtt.ls.readValue()>40) && (testtt.cs.getColorID() == 7)
-				);
-//		return true;
+		return (((Main.ls.readValue() < 40) && (Main.cs.getColorID() != 7)
+					|| 
+				(Main.ls.readValue() > 40) && (Main.cs.getColorID() == 7))
+				&&
+				(!Main.adjusted));
 	}
 
 }
