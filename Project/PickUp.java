28caa23f8c388631e507bedd8415c9b274844709
tomsoal <33701079+tomsoal@users.noreply.github.com>
2017-12-05 15:01:07 +0000

    Update PickUp.java
    Functionality is virtually complete, robot searches and picks up fine

--- Project/PickUp.java
+++ Project/PickUp.java
@@ -1,27 +1,26 @@
+import java.io.File;
+
 import lejos.nxt.Motor;
+import lejos.nxt.Sound;
 import lejos.robotics.subsumption.Behavior;
 import lejos.util.Delay;
 
 public class PickUp implements Behavior {
-	
-
-	
+	private static File file  = new File("getover.wav");
 	public void action() {
-		System.out.println("up");
-		Main.pilot.forward();
-		Motor.B.setSpeed(360);
-		Motor.B.rotate(140,true);
-		Main.pilot.stop();
-		System.out.println((int)Main.pp.getPose().getX()/50);
-		System.out.println((int)Main.pp.getPose().getY()/50);
-		Delay.msDelay(2000);
-		Main.map[(int)Main.pp.getPose().getX()/Main.scale][(int)Main.pp.getPose().getY()/Main.scale]=0;
-		Main.pickUp=true;	
+		System.out.println("up"); 
+		Motor.B.setSpeed(200);
+		Sound.playSample(file);
+		Main.pilot.travel(80);
+		Motor.B.rotate(180);
+		//Main.map[(int)Main.pp.getPose().getY()/Main.scale][(int)Main.pp.getPose().getX()/Main.scale]=0;
+		Main.pickUp=true;
 	}
 	public void suppress() {
 		
 	}
 	public boolean takeControl() {
-		return Main.us.getDistance()<20;
+		return ((Main.us.getDistance()<10)&&(!Main.pickUp)||(Main.atBase && Main.isCollected));
 	}
 }
+
